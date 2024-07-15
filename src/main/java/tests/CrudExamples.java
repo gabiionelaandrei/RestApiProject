dde5c91786a4d8058aeaec5b2db4c4df87f16405
gabiionelaandrei <andrei.gabi.ionela@gmail.com>
2024-07-15 20:37:01 +0300

    curs 33 CRUD operations

---
+++ src/main/java/tests/CrudExamples.java
@@ -0,0 +1,118 @@
+package tests;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.github.javafaker.Faker;
+import io.restassured.response.Response;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import static org.testng.Assert.assertEquals;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+/*
+ * POST
+ * https://keytodorestapi.herokuapp.com/api/save
+ * 
+ * GET
+ * https://keytodorestapi.herokuapp.com/api/:id
+ * 
+ * GET ALL
+ * https://keytodorestapi.herokuapp.com/api
+ * 
+ * PUT
+ * https://keytodorestapi.herokuapp.com/api/todos/:id
+ * 
+ * DELETE
+ * https://keytodorestapi.herokuapp.com/api/delete/:id
+ * 
+ * pana la .com este domeniu si este neschimbat el poate fi pus
+ * 
+ * String url = " https://keytodorestapi.herokuapp.com"
+ * .post(url + "api/save") -> nu este varianta fav lui restassured
+ * 
+ * Rest assured are o constanta pt URL
+ * 
+ */
+
+public class CrudExamples {
+	JSONObject requestBody, requestBodyUpdate;
+	String id;
+	
+	@SuppressWarnings("unchecked")
+	@BeforeClass
+	public void setup() {
+		
+		RestAssured.baseURI ="https://keytodorestapi.herokuapp.com/";
+		requestBody =new JSONObject();
+		requestBodyUpdate =new JSONObject();
+		Faker fake = new Faker();
+		requestBody.put("title", fake.food().dish());
+		requestBody.put("body", fake.chuckNorris().fact());
+		
+		requestBodyUpdate.put("title", fake.food().dish());
+		requestBodyUpdate.put("body", fake.chuckNorris().fact());
+			
+	}
+	@Test(priority=1)
+	public void createToDo() {
+		//in doau metode cu obiectul response in exemplu din clasa anteriora saucum e mai jos
+		
+	Response response =	
+		given().
+		//header ul are o prescurtare in reastassured
+		//header("Content-Type","application/json")
+		contentType(ContentType.JSON).
+		body(requestBody.toJSONString()).
+		//when(). este pus doar sa completeze sintaxa
+		post("api/save").
+		then().
+		statusCode(200).
+		body("info", equalTo("Todo saved! Nice job!")).
+		body("id", anything()).
+		log().all().extract().response();
+		id = response.jsonPath().getString("id");
+		System.out.println(id);	
+	}
+	
+	@Test (priority=2)
+	public void getToDo() {
+		Response response = given().get("api/" + id).then()
+				.statusCode(200)
+				.extract().response();
+		
+		System.out.println(response.asPrettyString());
+		System.out.println(response.statusCode());
+		System.out.println(response.jsonPath().getString("_id"));
+		//assertul test ng
+		assertEquals(id,response.jsonPath().getString("_id") );
+		//hamcrest assert
+		assertThat(id,is(equalTo(response.jsonPath().getString("_id"))));
+	}
+	
+	@Test (priority=3)
+	public void UpdateToDo() {
+		
+		Response response = given().
+				body(requestBodyUpdate.toJSONString()).
+                put("api/todos/"+id).then().extract().response();
+		
+		System.out.println(response.asPrettyString());
+		System.out.println(requestBodyUpdate.toJSONString());
+	}
+	
+	@Test (priority=4)
+	public void DeleteToDo() {
+		Response response = given().delete("api/delete/" + id).then()
+				.statusCode(200)
+				.extract().response();
+		
+		System.out.println(response.asPrettyString());
+		System.out.println(response.statusCode());
+		System.out.println(response.jsonPath().getString("_id"));
+		
+	}
+	
+}
--- src/main/java/tests/RestExample.java
+++ src/main/java/tests/RestExample.java
@@ -1,23 +1,49 @@
 package tests;
 
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
+
+import com.github.javafaker.Faker;
 
 import io.restassured.response.Response;
 import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import java.io.File;
 public class RestExample {
 	
 	
+	@SuppressWarnings("unchecked")
 	@Test
 	public void restExampleTest() {
 		
+		Faker fakeData = new Faker();
+		
+		
+		JSONObject requestBody = new JSONObject();
+		requestBody.put("name", fakeData.artist().name());
+		requestBody.put("email",fakeData.internet().emailAddress());
+		requestBody.put("age", "33");
+		requestBody.put("gender", "M");
+		File fisier = new File("data.json");
+		
+		//daca aveam folder dadeam pathul 
 		Response response = given()
 				.header("Content-Type","application/json")
-				.body("{\r\n"
-						+ "    \"name\": \"Gabi\",\n"
-						+ "    \"email\": \"email@db.com\",\n"
-						+ "    \"age\": \"22\",\n"
-						+ "    \"gender\": \"f\"\n"
-						+ "}")
+				// ex 1 --> direct in body as String
+				/*
+				 *.body("{\r\n"
+					+ "    \"name\": \"Gabi\",\n"
+					+ "    \"email\": \"email@db.com\",\n"
+					+ "    \"age\": \"22\",\n"
+					+ "    \"gender\": \"f\"\n"
+					+ "}")
+				 */
+				//ex 2 --> body as JsonObject
+				
+				//.body(requestBody.toJSONString())
+				//ex3 --> body as file
+				.body(fisier)
 				.when()
 				.post("https://keytrcrud.herokuapp.com/api/users/")
 				.then()
@@ -26,8 +52,11 @@ public class RestExample {
 		System.out.println(response);
 		System.out.println(response.asString());
 		System.out.println(response.asPrettyString());
-		
-		
+		String msg = response.jsonPath().getString("msg");
+		System.out.println(msg);
+		assertEquals(msg, "Successfully added!");
+		String email = response.jsonPath().getString("result.email");
+		System.out.println(email);
 	}
 	
 	
